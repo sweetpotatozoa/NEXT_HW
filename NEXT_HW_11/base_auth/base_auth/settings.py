5df6b32942647879462f58832a7d221ad45a3b13
김상우 <tkddn3200@korea.ac.kr>
2024-07-22 15:58:48 +0900

    Update settings.py

--- NEXT_HW_11/base_auth/base_auth/settings.py
+++ NEXT_HW_11/base_auth/base_auth/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-ms!e8yw15s%e@e7%z2v-1zbxh8vc_-^mj_tl4x%lof8)98+eet
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
